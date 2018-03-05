da9d86654a778ae94a45f59acd7edc93afcbe65d
Andy Edwards <jedwards@fastmail.com>
2018-03-05 15:51:18 -0600

    ci(solano): attempt to get postgres working

--- env/test.js
+++ env/test.js
@@ -1,5 +1,5 @@
 exports.DB_HOST = 'localhost'
-exports.DB_PASSWORD = 'password'
-exports.DB_USER = 'postgres'
-exports.DB_NAME = 'graphql_sequelize_extra'
+exports.DB_PASSWORD = process.env.TDDIUM_DB_NAME || 'password'
+exports.DB_USER = process.env.TDDIUM_DB_USER || 'postgres'
+exports.DB_NAME = process.env.TDDIUM_DB_NAME || 'graphql_sequelize_extra'
 
